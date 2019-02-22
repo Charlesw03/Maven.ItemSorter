f7d501712bd08df348fc0b36c769868ee2c009f9
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-02-22 07:56:07 -0500

    completed item sorter big shout out to Eugene and davis

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,19 @@
 package rocks.zipcode.io.comparators;
 
+
+  import rocks.zipcode.io.Item;
+
+  import java.util.Comparator;
+
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+        public int compare(Item object1,Item object2){
+            return (int) (object1.getId()-object2.getId());
+
+        }
+
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,24 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    public int compare(Item object1, Item object2) {
+
+        if (object1.getPrice() < object2.getPrice()) {
+            return -1;
+        }
+
+        if(object1.getPrice() > object2.getPrice()){
+            return 1;
+        }
+        return 0;
+
+    }
+
 }
